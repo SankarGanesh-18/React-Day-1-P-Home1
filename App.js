de9220cbd5d1dffb9124abaa78f147d38b307f8a
SankarGanesh-18 <122220555+SankarGanesh-18@users.noreply.github.com>
2023-03-29 12:10:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Team from './Team/Team'; 
+function App() {
+  return (
+    <div className="App">
+      Team
+      <Team/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Team.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Team extends Component {
+  render() {
+    return (
+      <div>This is a Team Functional Component</div>
+    )
+  }
+}
+
+export default Team
